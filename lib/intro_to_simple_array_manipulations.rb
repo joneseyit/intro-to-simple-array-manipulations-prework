736a7bc8c6c27486438b675421aeb98f1505066a
joneseyit <joneseyit@protonmail.com>
2018-10-18 22:32:49 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,13 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-/
+def using_insert(array, el)
+  array.insert(4, el)
+end
+
+def using_uniq(array)
+  array.uniq
+end
 
 # def using_flatten(array)
 #   array.flatten
